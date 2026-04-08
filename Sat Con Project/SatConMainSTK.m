b3d2434a9b304dc082cf3d4bbef4acec5efa7f9f
evanpaull1 <evancpaull@gmail.com>
2026-04-07 22:09:20 -0400

    Actually gets values now

--- Sat Con Project/SatConMainSTK.m
+++ Sat Con Project/SatConMainSTK.m
@@ -4,6 +4,8 @@
 % Author: Evan Paull
 clc; clear; close all
 
+X = [n, inclination, RAAN_spacing];
+
 %Create a new STK instance (all properties stored in 'STK' struct)
 STK.app = actxserver('STK13.application');
 STK.app.Visible = 1;
@@ -15,50 +17,20 @@ STK.scenarioPath = 'C:\Users\evanc\Documents\STK_ODTK 13\SatCon\SatCon.sc';
 STK.root.Load(STK.scenarioPath);
 scenario = STK.root.CurrentScenario;
 
-% Specify the time interval
-startTime = STK.root.CurrentScenario.StartTime;
-stopTime  = STK.root.CurrentScenario.StopTime;
-%timeStep = STK.root.CurrentScenario.Interval
-timeStep = 60;
-
 %Access Relevant Objects
 satCon = STK.root.CurrentScenario.Children.Item('iAmTheConstellationNow');
 littleBad = STK.root.CurrentScenario.Children.Item('LittleBad');
 reallyBad = STK.root.CurrentScenario.Children.Item('ReallyBad');
 priority = STK.root.CurrentScenario.Children.Item('Priority');
 
-%Compute Acesses
-littleBad.ComputeAccesses();
-reallyBad.ComputeAccesses();
-priority.ComputeAccesses();
+times = runSTK(); %Compute Accesses and Extract Coverage Durations
 
-littleBad.ExportAccessesAsText('C:\Users\evanc\Documents\LittleBadAccess.txt');
-dataLB = readtable('C:\Users\evanc\Documents\LittleBadAccess.txt');
-disp(dataLB);
+%CALCULATE FITNESS
 
-%Extract Access Intervals and Durations
-dpLB = littleBad.DataProviders.Item('Access Duration');
-%dpRB = reallyBad.DataProviders.Item('Access Duration');
-%dpP  = priority.DataProviders.Item('Access Duration');
+%RUN GA
+    %Update constellation function
 
-%dpElementLB = dpLB.ExecElements();
-%dpElementRB = dpRB.Exec();
-%dpElementP  = dpP.Exec();
-
-% See exactly what fields are returned
-for j = 0 : dpElementLB.DataSets.Count - 1
-    disp(dpElementLB.DataSets.Item(j).Name);
-end
-
-% Check children of the coverage definition
-for i = 0 : littleBad.Children.Count - 1
-    child = littleBad.Children.Item(i);
-    fprintf('%s (%s)\n', child.InstanceName, child.ClassName);
-end
-
-%durationLB = cell2mat(dpElmementLB.DataSets.GetDataSetByName('Duration').GetValues);
-%durationRB = cell2mat(dpElementRB.DataSets.GetDataSetByName('Duration').GetValues);
-%durationP  = cell2mat(dpElementP.DataSets.GetDataSetByName('Duration').GetValues);
+%OUTPUT RESULTS
 
 %Leave and close STK
 STK.root.CloseScenario();
---
+++ Sat Con Project/runSTK.m
@@ -0,0 +1,27 @@
+function [coverageTimes] = runSTK()
+    % This function runs the STK propagation by computing accesses and 
+    % returns the coverage time for the three coverage definitions.
+
+    %Compute Acesses
+    littleBad.ComputeAccesses();
+    reallyBad.ComputeAccesses();
+    priority.ComputeAccesses();
+    
+    %Get Data Providers
+    dpLB = littleBad.DataProviders.Item('Access Duration');
+    dpRB = reallyBad.DataProviders.Item('Access Duration');
+    dpP  = priority.DataProviders.Item('Access Duration');
+    resultsLB = dpLB.Exec;
+    resultsRB = dpRB.Exec;
+    resultsP  = dpP.Exec;
+
+    %Extract total coverage time
+    durationLB = resultsLB.DataSets.GetDataSetByName('Duration').GetValues;
+    durationLB = sum(cell2mat(durationLB));
+    durationRB = resultsRB.DataSets.GetDataSetByName('Duration').GetValues;
+    durationRB = sum(cell2mat(durationRB));
+    durationP  = resultsP.DataSets.GetDataSetByName('Duration').GetValues;
+    durationP  = sum(cell2mat(durationP));
+
+    coverageTimes = [durationLB, durationRB, durationP];
+end
